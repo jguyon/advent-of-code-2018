e881c5a8d8de44505fbe9bf0d6fe740e88e93019
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-12-03 14:25:32 +0100

    Solve day 1 part 2

---
+++ day01-2.js
@@ -0,0 +1,56 @@
+// @flow
+
+import * as readline from "readline";
+
+const getInputLines = () =>
+  new Promise(resolve => {
+    const lines: string[] = [];
+    const rl = readline.createInterface({ input: process.stdin });
+
+    rl.on("line", (nextLine: string) => {
+      lines.push(nextLine);
+    }).on("close", () => {
+      resolve(lines);
+    });
+  });
+
+const parseChanges = (lines: string[]) =>
+  lines.map(line => {
+    const change = parseInt(line, 10);
+
+    if (isNaN(change)) {
+      throw new Error(`"${line.trim()}" is not a valid integer`);
+    }
+
+    return change;
+  });
+
+const findRepeatFreq = (changes: number[]) => {
+  const prevFreqs = [0];
+
+  while (true) {
+    for (const change of changes) {
+      const nextFreq = prevFreqs[prevFreqs.length - 1] + change;
+
+      if (prevFreqs.includes(nextFreq)) {
+        return nextFreq;
+      }
+
+      prevFreqs.push(nextFreq);
+    }
+  }
+};
+
+console.log("Input frequencies then press <ctrl-D>:");
+
+getInputLines()
+  .then(parseChanges)
+  .then(changes => {
+    console.log("Calculating...");
+    return changes;
+  })
+  .then(findRepeatFreq)
+  .then(
+    repeatFreq => console.log("First repeated frequency:", repeatFreq),
+    error => console.error(error)
+  );
