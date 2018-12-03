1afe79eb8671f81f70588e459343dcf1bf52ec1d
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-12-03 14:17:58 +0100

    Refactor day 1 part 1

--- day01-1.js
+++ day01-1.js
@@ -2,22 +2,38 @@
 
 import * as readline from "readline";
 
-const addFreq = (prevFreq: number, nextLine: string) => {
-  const nextFreq = parseInt(nextLine, 10);
+const getInputLines = () =>
+  new Promise(resolve => {
+    const lines: string[] = [];
+    const rl = readline.createInterface({ input: process.stdin });
 
-  if (isNaN(nextFreq)) {
-    throw new Error("invalid input");
-  }
-
-  return prevFreq + nextFreq;
-};
-
-let freq = 0;
-readline
-  .createInterface({ input: process.stdin })
-  .on("line", (line: string) => {
-    freq = addFreq(freq, line);
-  })
-  .on("close", () => {
-    console.log("frequency:", freq);
+    rl.on("line", (nextLine: string) => {
+      lines.push(nextLine);
+    }).on("close", () => {
+      resolve(lines);
+    });
   });
+
+const parseChanges = (lines: string[]) =>
+  lines.map(line => {
+    const change = parseInt(line, 10);
+
+    if (isNaN(change)) {
+      throw new Error(`"${line.trim()}" is not a valid integer`);
+    }
+
+    return change;
+  });
+
+const calcFreq = (changes: number[]) =>
+  changes.reduce((freq, change) => freq + change, 0);
+
+console.log("Input frequencies then press <ctrl-D>:");
+
+getInputLines()
+  .then(parseChanges)
+  .then(calcFreq)
+  .then(
+    freq => console.log("Final frequency:", freq),
+    error => console.error(error)
+  );
